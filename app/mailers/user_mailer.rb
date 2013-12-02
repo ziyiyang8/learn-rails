9aa8cc880b61c4d87af71fea6fc542a80ada3fd5
Ziyi Yang <ziyiyang8@gmail.com>
2013-12-01 17:15:18 -0800

    send mailer

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,8 @@
+class UserMailer < ActionMailer::Base
+  default from: "do-notreply@example.com"
+
+  def contact_email(contact)
+  	@contact = contact
+  	mail(to: ENV["OWNER_EMAIL"], from: @contact.email, :subject => "Website Contact")
+  end
+end
